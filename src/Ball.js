edba3efd3f2dea63535af642c13714ba98fe6d6d
shawn <shawnantonucci2018@gmail.com>
2019-04-04 10:56:39 -0700

    setup RNE

--- src/Ball.js
+++ src/Ball.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import { View, Animated } from "react-native";
 
 class Ball extends Component {
-  componentWillMount() {
+  constructor(props) {
+    super(props);
+    
     this.position = new Animated.ValueXY(0, 0);
     Animated.spring(this.position, {
       toValue: { x: 200, y: 500 }
---
+++ src/Deck.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { View, Animated } from 'react-native';
+
+class Deck extends Component {
+    // constructor(props) {
+    //     super(props);
+
+    // }
+
+    renderCards = () => {
+        return this.props.data.map(item => {
+            return this.props.renderCard(item);
+        })
+    }
+
+    render() {
+        return (
+            <View>
+                {this.renderCards()}
+            </View>
+        )
+    }
+}
+
+export default Deck;
